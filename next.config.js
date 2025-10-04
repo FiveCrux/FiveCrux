1808779dae266b588f0ba7c803e8e6b0dd83e05a
sidak <gdsd1524@gmail.com>
2025-10-05 02:52:56 +0530

    .

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  experimental: {
+    serverComponentsExternalPackages: ['drizzle-orm', 'pg', 'postgres', 'framer-motion', 'recharts']
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('drizzle-orm', 'pg', 'postgres', 'framer-motion', 'recharts')
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
